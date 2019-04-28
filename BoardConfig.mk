98f71b87377eeed33014b06e76edbbea4648f127
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    olivelite: releasetools: Ship and update vbmeta image
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ BOARD_USES_QCOM_HARDWARE := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # SELinux
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
