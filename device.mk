f3a52145b555ed95cf90b2eea0ebdff51680bda4
ZeeLog <Danjel.Zammit@gmail.com>
2019-12-27 22:44:41 +0100

    olivelite: Build our own light HAL
    Change-Id: Id0ea4dbf5ac922abca059738f41f27b1f54c32a9
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_olive
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common
