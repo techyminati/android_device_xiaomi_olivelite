4e7e7e8665d5c42d77cd99e89c11e33dfee49832
Dan Willemsen <dwillemsen@google.com>
2021-04-27 21:58:11 +0530

    olivelite: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES Bug: 77599627 Test: lunch  [see entries in list] Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_olivelite.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_olivelite-user \
+    lineage_olivelite-userdebug \
+    lineage_olivelite-eng
